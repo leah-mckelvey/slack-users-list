9cb448dc8413ed8a89f8acf7ee9bbc9730d82ddb
leah-mckelvey <leahofrivendell@gmail.com>
2016-03-29 12:48:53 -0700

    Skeleton of app

---
+++ app/src/main/java/com/interview/leah/slacklist/UserListApplication.java
@@ -0,0 +1,12 @@
+package com.interview.leah.slacklist;
+
+import android.app.Application;
+
+/**
+ * Created by leah on 3/28/16.
+ */
+public class UserListApplication extends Application {
+    @Override
+    public void onCreate() {
+    }
+}
